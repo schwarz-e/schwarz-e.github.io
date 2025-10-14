2e3af12a9e18c8029d5c7efd8f27a498c021e8f1
Erica Schwarz <erica.schwarz@yale.edu>
2025-10-14 13:49:47 -0400

    adding auto-cite

--- assets/update_publications.py
+++ assets/update_publications.py
@@ -1,5 +1,6 @@
 from scholarly import scholarly
 import yaml
+import time
 
 scholar_id = "s4mUv1AAAAAJ"
 author = scholarly.search_author_id(scholar_id)
@@ -7,13 +8,22 @@ author = scholarly.fill(author, sections=["publications"])
 
 pubs = []
 for p in author["publications"]:
+    # Fill each publication to get full metadata
+    pub_filled = scholarly.fill(p)
+    bib = pub_filled.get("bib", {})
+    
     pubs.append({
-        "title": p["bib"]["title"],
-        "authors": p["bib"].get("author", ""),
-        "year": p["bib"].get("pub_year", ""),
-        "venue": p["bib"].get("venue", ""),
-        "url": p.get("pub_url", ""),
+        "title": bib.get("title", ""),
+        "authors": bib.get("author", ""),   # now populated
+        "year": bib.get("pub_year", ""),
+        "venue": bib.get("venue", bib.get("journal", "")),
+        "url": pub_filled.get("pub_url", ""),
     })
+    
+    time.sleep(1)  # small delay to avoid Google blocking requests
 
-with open("_data/publications.yml", "w") as f:
-    yaml.dump(pubs, f, allow_unicode=True)
+# Save to YAML
+with open("_data/publications.yml", "w", encoding="utf-8") as f:
+    yaml.dump(pubs, f, allow_unicode=True, sort_keys=False)
+
+print(f"✅ Saved {len(pubs)} publications to _data/publications.yml")
